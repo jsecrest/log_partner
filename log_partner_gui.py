383e78870dd8163a789b59b4bc9ac143fbc3d948
Jason Secrest <jlsecrest@gmail.com>
2018-06-13 20:30:30 -0600

    Initial code
    Lets be honest, it's just a hello world from the website.

---
+++ log_partner_gui.py
@@ -0,0 +1,16 @@
+
+import kivy
+kivy.require('0.1.4')
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+
+class MyApp(App):
+
+    def build(self):
+        return Label(text='Hello world')
+
+
+if __name__ == '__main__':
+    MyApp().run()
